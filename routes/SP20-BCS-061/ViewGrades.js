830ae8759358c439425ff367afda7cbac83294e2
rshahmeer27 <shahmeerbhatti27@gmail.com>
2022-12-26 00:26:02 +0500

    SP20-BCS-083

--- routes/SP20-BCS-061/ViewGrades.js
+++ routes/SP20-BCS-061/ViewGrades.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Student = require('../../models/student');
 
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
-});
 router.get('/grades/:studentId', function (req, res, next) {
 	Student.find({ _id: req.params.studentId }).exec(function (error, results) {
 		if (error) {
