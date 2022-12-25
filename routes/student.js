c1dc7cc20b3c293f049e8338fc157e4091055d4e
RajaRehman054 <abdurrehman283560@gmail.com>
2022-12-25 14:56:58 +0500

    Attempt Quiz Route in Sp20-BCS-054

--- routes/student.js
+++ routes/student.js
@@ -6,17 +6,4 @@ router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 });
 
-router.post('/attempt-quiz', function (req, res) {
-	let numCorrect = 0;
-	for (let i = 0; i < req.body.length; i++) {
-		if (req.body[i].submittedAnswer === req.body[i].correctAnswer) {
-			numCorrect++;
-		}
-	}
-	res.send({
-		numCorrect: numCorrect,
-		numQuestions: req.body.length,
-	});
-});
-
 module.exports = router;
