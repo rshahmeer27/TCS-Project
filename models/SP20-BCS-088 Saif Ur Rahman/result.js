0b90a3e70014e120bc349fc42c0373f69ff625de
SaifAlveyJ <saif.alvey@gmail.com>
2022-12-25 16:54:57 +0500

    Route: Result of Classes, Model: Result Schema

---
+++ models/SP20-BCS-088 Saif Ur Rahman/result.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var schema = mongoose.Schema;
+
+var resultSchema = new schema({
+    courseId : {
+        type : String,
+        required : true,
+    },
+    courseName : String,
+    teacherName : {
+        type : String,
+        required : true,
+    },
+    totalMarks : Number,
+    obtMarks : Number,
+    gpa : Number,
+})
+
+export default mongoose.model('Result', resultSchema);
