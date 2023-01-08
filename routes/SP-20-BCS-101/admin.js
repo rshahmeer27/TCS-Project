c2ca36c25aad37982d92a0b320f2a2ab1f20975f
Uzair-Sohail <121401366+Uzair-Sohail@users.noreply.github.com>
2023-01-08 23:29:28 +0500

    Update admin.js

--- routes/SP-20-BCS-101/admin.js
+++ routes/SP-20-BCS-101/admin.js
@@ -1,13 +1,6 @@
-const express = require("express");
-const app = express();
-
-app.get("/admin/dashboard", function (req, res) {
-  // check if the user is an admin
-  if (!req.user.isAdmin) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  // render the admin dashboard page
-  res.render("admin/dashboard");
+router.post('/:id/addclass', async (req, res) => {
+  const student = await Student.findById(req.params.id);
+  student.classes.push(req.body.class);
+  await student.save();
+  res.send(student);
 });
